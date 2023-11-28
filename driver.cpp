bae53108245a5c09fb83244931e271272ac916b9
SamMouradian <113820652+1105-Mouradian-Samuel@users.noreply.github.com>
2023-11-28 04:00:37 +0000

    Version 2.0

--- driver.cpp
+++ driver.cpp
@@ -1,7 +1,7 @@
 //Programming Project 3 - Due 11.30.2023
 //Computer Science 219 - Seciton 1001
 //Author - Samuel Mouradian
-//Version - 1.0
+//Version - 2.0
 
 
 #include <iostream>
@@ -9,10 +9,10 @@
 using namespace std;
 
 
-#define FILENAME "Programming-Project-2.txt"
-#define MAXLINE 40
+#define FILENAME "Programming-Project-3.txt"
+#define MAXLINE 15
 
-//Operations:
+//Operations - Programming Project 2:
 void ADD_ADDS(ifstream&, char);
 void AND_ANDS(ifstream&, char);
 void ASR_ASRS(ifstream&, char);
@@ -23,54 +23,118 @@ void ORR_ORRS(ifstream&, char);
 void SUB_SUBS(ifstream&, char);
 void XOR_XORS(ifstream&, char);
 
+
+//Operations - Programming Project 3:
+void MOV(string, long int, long int, long int, long int, long int, long int, long int, long int, long int, int, int, int, int);
+
 int main(){
     ifstream txtFile;
     txtFile.open(FILENAME);
 
     char e;
+    long int r0, r1, r2, r3, r4, r5, r6, r7;
+    int N, Z, C, V;
 
     for(int i = 0; i < MAXLINE; i++){
         if(txtFile.good()){
-            long int hexVal1, hexVal2;
-            string operand;
+            long int hexVal1, hexVal2, movVar;
+
+            string operand, regWrite, regRead, regPull;
+            
+            int shift;
 
             txtFile >> operand;
 
-            if(operand == "ADD" || operand == "ADDS"){
+            if(operand == "ADD" || operand == "ADDS" || operand == "add" || operand == "adds"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> regPull;
+                cout << " " << regWrite << " " << regRead << " " << regPull << endl;
+
                 ADD_ADDS(txtFile, e);
             }
-            else if(operand == "AND" || operand == "ANDS"){
+            else if(operand == "AND" || operand == "ANDS" || operand == "and" || operand == "ands"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> regPull;
+                cout << " " << regWrite << " " << regRead << " " << regPull << endl;
+
                 AND_ANDS(txtFile, e);
             }
-            else if(operand == "ASR" || operand == "ASRS"){
+            else if(operand == "ASR" || operand == "ASRS" || operand == "asr" || operand == "asrs"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> shift;
+                cout << " " << regWrite << " " << regRead << " " << shift << endl;
+
                 ASR_ASRS(txtFile, e);
             }
-            else if(operand == "LSR" || operand == "LSRS"){
+            else if(operand == "LSR" || operand == "LSRS" || operand == "lsr" || operand == "lsrs"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> shift;
+                cout << " " << regWrite << " " << regRead << " " << shift << endl;
+
                 LSR_LSRS(txtFile, e);
             }
-            else if(operand == "LSL" || operand == "LSLS"){
+            else if(operand == "LSL" || operand == "LSLS" || operand == "lsl" || operand == "lsls"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> shift;
+                cout << " " << regWrite << " " << regRead << " " << shift << endl;
+
                 LSL_LSLS(txtFile, e);
             }
-            else if(operand == "NOT" || operand == "NOTS"){
-                cout << operand;
+            else if(operand == "NOT" || operand == "NOTS" || operand == "not" || operand == "nots"){
+                cout << operand << endl;
                 NOT_NOTS(txtFile, e);
             }
-            else if(operand == "ORR" || operand == "ORRS"){
+            else if(operand == "ORR" || operand == "ORRS" || operand == "orr" || operand == "orrs"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> regPull;
+                cout << " " << regWrite << " " << regRead << " " << regPull << endl;
+
                 ORR_ORRS(txtFile, e);
             }
-            else if(operand == "SUB" || operand == "SUBS"){
+            else if(operand == "SUB" || operand == "SUBS" || operand == "sub" || operand == "subs"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> regPull;
+                cout << " " << regWrite << " " << regRead << " " << regPull << endl;
+
                 SUB_SUBS(txtFile, e);
             }
-            else if(operand == "XOR" || operand == "XORS"){
+            else if(operand == "XOR" || operand == "XORS" || operand == "xor" || operand == "xors"){
                 cout << operand;
+                txtFile >> regWrite >> regRead >> regPull;
+                cout << " " << regWrite << " " << regRead << " " << regPull << endl;
+
                 XOR_XORS(txtFile, e);
+            }
+            else if(operand == "MOV" || operand == "mov"){
+                cout << operand << " ";
+                txtFile >> regWrite >> hex >> movVar;
+                cout << regWrite << " " << "0x" << hex << movVar << endl;
+                
+                if(regWrite == "R0," || regWrite == "r0,"){
+
+                }
+                else if(regWrite == "R1," || regWrite == "r1,"){
+
+                }
+                else if(regWrite == "R2," || regWrite == "r2,"){
+
+                }
+                else if(regWrite == "R3," || regWrite == "r3,"){
+
+                }
+                else if(regWrite == "R4," || regWrite == "r4,"){
+
+                }
+                else if(regWrite == "R5," || regWrite == "r5,"){
+
+                }
+                else if(regWrite == "R6," || regWrite == "r6,"){
+
+                }
+                else if(regWrite == "R7," || regWrite == "r7,"){
+
+                }
+                MOV(regWrite, movVar, r0, r1, r2, r3, r4, r5, r6, r7, N, Z, C, V);
             }
         }
     }
@@ -91,6 +155,7 @@ void ADD_ADDS(ifstream& hexFile, char end){
 
 void AND_ANDS(ifstream& hexFile, char end){
     long int hexVal1, hexVal2;
+    string r1, r2, r3, r4, r5, r6, r7;
 
     hexFile >> hex >> hexVal1;
     hexFile >> hex >> hexVal2;
@@ -201,3 +266,44 @@ void XOR_XORS(ifstream& hexFile, char end){
 
     cout << "Result:    0x" << hex << (hexVal1 ^ hexVal2) << endl << endl;
 }
+
+void MOV(string writeToReg, long int hexVal, long int reg0, long int reg1, long int reg2, long int reg3, long int reg4, long int reg5, long int reg6, long int reg7, int neg, int zero, int carry, int overflow){
+    long int r0, r1, r2, r3, r4, r5, r6, r7;
+    int N, Z, C, V;
+
+    if(writeToReg == "R0," || writeToReg == "r0,"){
+    }
+    else if(writeToReg == "R1," || writeToReg == "r1,"){
+    }
+    else if(writeToReg == "R2," || writeToReg == "r2,"){
+    }
+    else if(writeToReg == "R3," || writeToReg == "r3,"){
+    }
+    else if(writeToReg == "R4," || writeToReg == "r4,"){
+    }
+    else if(writeToReg == "R5," || writeToReg == "r5,"){
+    }
+    else if(writeToReg == "R6," || writeToReg == "r6,"){
+    }
+    else if(writeToReg == "R7," || writeToReg == "r7,"){
+    }
+}
+
+/*r0 = hexVal;
+        r1 = reg1;
+        r2 = reg2;
+        r3 = reg3;
+        r4 = reg4;
+        r5 = reg5;
+        r6 = reg6;
+        r7 = reg7;
+
+        N = neg;
+        Z = zero;
+        C = carry;
+        V = overflow;
+
+        cout << "R0: 0x" << hex << r0 << "  R1: 0x" << hex << r1 << "  R2: 0x" << hex << r2 << "  R3: 0x" << hex << r3 << "  R4: 0x" << hex << r4 << "  R5: 0x";
+        cout << hex << r5 << "  R6: 0x" << hex << r6 << "  R7: 0x" << hex << r7 << endl;
+
+        cout << "N = " << N << "  Z = " << Z << "  C = " << C << "  V = " << V << endl << endl;*/
--- makefile
+++ makefile
@@ -1,8 +1,8 @@
-PA3v1: driver.o
-	g++ -o PA3v1 driver.o
+PA3v2: driver.o
+	g++ -o PA3v2 driver.o
 
 driver.o: driver.cpp
 	g++ -c driver.cpp
 
 clean:
-	rm *.o PA3v1
+	rm *.o PA3v2
