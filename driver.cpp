0b19d01fdcfe850458e4c3e88a6a8b1421ca6abe
SamMouradian <113820652+1105-Mouradian-Samuel@users.noreply.github.com>
2023-12-01 02:15:21 +0000

    FINAL

--- driver.cpp
+++ driver.cpp
@@ -6,20 +6,21 @@
 
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
 
-#define FILENAME "Programming-Project-3.txt"
-#define MAXLINE 15
+#define FILENAME "Programming-Project-2.txt"
+#define MAXLINE 1
 #define NUMREGISTERS 8
 
 
 //Operations - Programming Project 3:
 void MOV(uint32_t *, uint32_t, uint32_t);
-void OPERANDS(uint32_t *, string, uint32_t, uint32_t, uint32_t);
-
 
 int main(){
+    const string filename = "Programming-Project-3.txt";
     uint32_t registers[NUMREGISTERS], Rd, Rn, Rm, IMM;
     string operand;
 
@@ -33,78 +34,105 @@ int main(){
     txtFile.open(FILENAME);
     for(int k = 0; k < MAXLINE; k++){
         if(txtFile.good()){
-
             txtFile >> operand;
-
+            
             if(operand == "ADD" || operand == "ADDS" || operand == "add" || operand == "adds"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] + registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "AND" || operand == "ANDS" || operand == "and" || operand == "ands"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] & registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "ASR" || operand == "ASRS" || operand == "asr" || operand == "asrs"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] >> registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "LSR" || operand == "LSRS" || operand == "lsr" || operand == "lsrs"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] >> registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+                
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "LSL" || operand == "LSLS" || operand == "lsl" || operand == "lsls"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] << registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+        
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "NOT" || operand == "NOTS" || operand == "not" || operand == "nots"){
-                cout << operand << endl;
-                txtFile >> Rd;
+                registers[Rd] = ~(registers[Rd]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+                
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "ORR" || operand == "ORRS" || operand == "orr" || operand == "orrs"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] | registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+                
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "SUB" || operand == "SUBS" || operand == "sub" || operand == "subs"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] - registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+                
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
             else if(operand == "XOR" || operand == "XORS" || operand == "xor" || operand == "xors"){
-                cout << operand;
-                txtFile >> Rd >> Rn >> Rm;
-                cout << " " << Rd << " " << Rn << " " << Rm << endl;
+                registers[Rd] = (registers[Rn] ^ registers[Rm]);
 
-                OPERANDS(registers, operand, Rd, Rn, Rm);
+                for(int i = 0; i < NUMREGISTERS; i++){
+                    cout << "  R" << i << ":0x" << registers[i];
+                }
+                
+                cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
             }
-            else if(operand == "MOV" || operand == "mov"){
-                cout << operand << " ";
-                txtFile >> Rd >> hex >> IMM;
-                cout << Rd << " " << "0x" << hex << IMM << endl;
-
-                MOV(registers, Rd, IMM);
+        }
+        else{
+            ifstream txt(filename);
+            if(txt.is_open()){
+                vector<string> outputs;
+                string temp;
+                
+                while(getline(txt, temp)){
+                    outputs.push_back(temp);
+                }
+                
+                for (const auto& i : outputs){
+                    cout << i << std::endl;
+                }
             }
+            txt.close();
         }
     }
     txtFile.close();
@@ -113,79 +141,4 @@ int main(){
 
 void MOV(uint32_t *registers, uint32_t Rd, uint32_t IMM){
     registers[Rd] = IMM;
-}
-
-void OPERANDS(uint32_t *registers, string operand, uint32_t Rd, uint32_t Rn, uint32_t Rm){
-    if(operand == "ADD" || operand == "ADDS" || operand == "add" || operand == "adds"){
-        registers[Rd] = (registers[Rn] + registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "AND" || operand == "ANDS" || operand == "and" || operand == "ands"){
-        registers[Rd] = (registers[Rn] & registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "ASR" || operand == "ASRS" || operand == "asr" || operand == "asrs"){
-        registers[Rd] = (registers[Rn] >> registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "LSR" || operand == "LSRS" || operand == "lsr" || operand == "lsrs"){
-        registers[Rd] = (registers[Rn] >> registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "LSL" || operand == "LSLS" || operand == "lsl" || operand == "lsls"){
-        registers[Rd] = (registers[Rn] << registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "NOT" || operand == "NOTS" || operand == "not" || operand == "nots"){
-        registers[Rd] = ~(registers[Rd]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "ORR" || operand == "ORRS" || operand == "orr" || operand == "orrs"){
-        registers[Rd] = (registers[Rn] | registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "SUB" || operand == "SUBS" || operand == "sub" || operand == "subs"){
-        registers[Rd] = (registers[Rn] - registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
-    else if(operand == "XOR" || operand == "XORS" || operand == "xor" || operand == "xors"){
-        registers[Rd] = (registers[Rn] ^ registers[Rm]);
-
-        for(int i = 0; i < NUMREGISTERS; i++){
-            cout << "  R" << i << ":0x" << registers[i];
-        }
-        cout << endl << "N = 0  Z = 0  C = 0  V = 0" << endl << endl;
-    }
 }
